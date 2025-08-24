4d0b36fe9e6fb7b4ead1c2dfe990ecf9be530ed4
pallavkishor <pallavkishor01.com>
2025-08-24 23:22:44 +0530

    fix error

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
+import { queryClient } from "./lib/queryClient.ts";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { useAuth } from "@/hooks/useAuth";
-import Landing from "./pages/Landing";
-import Home from "./pages/Home";
-import EventDetails from "./pages/EventDeatils";
-import ContributorEvent from "./pages/ContributorEvent";
-import NotFound from "./pages/not-found";
-import InvitationHandler from "./pages/InvitationHandler";
+import { Toaster } from "@/components/ui/toaster.tsx";
+import { TooltipProvider } from "@/components/ui/tooltip.tsx";
+import { useAuth } from "@/hooks/useAuth.ts";
+import Landing from "./pages/Landing.tsx";
+import Home from "./pages/Home.tsx";
+import EventDetails from "./pages/EventDeatils.tsx";
+import ContributorEvent from "./pages/ContributorEvent.tsx";
+import NotFound from "./pages/not-found.tsx";
+import InvitationHandler from "./pages/InvitationHandler.tsx";
 
 function Router() {
   const { isAuthenticated, isLoading } = useAuth();
